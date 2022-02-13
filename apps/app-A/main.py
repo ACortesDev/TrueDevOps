5ce2a76ecb280cc6b29b8106a8b479e298dd853a
Álvaro Cortés <alvarocortesdevesa@gmail.com>
2022-02-13 21:18:48 +0100

    arstarst

--- apps/app-A/main.py
+++ apps/app-A/main.py
@@ -5,13 +5,19 @@ import time
 ANNOTATIONS_PATH = os.getenv("ANNOTATIONS_PATH")
 
 
-def get_feature_flags():
+def parse_annotation(annotation: str):
+    k, v = annotation.split("=")
+    return k, v.strip("\"")
+
+
+def get_feature_flags() -> dict:
     with open(ANNOTATIONS_PATH, "r") as file:
-        return [line for line in file]
+        annotations = [parse_annotation(line) for line in file]
+        feature_flags = filter(lambda x: "feature-" in x[0], annotations)
+        return dict(feature_flags)
 
 
 if __name__ == "__main__":
-    print("Hello World!")
     while True:
-        time.sleep(5)
+        time.sleep(1)
         print(get_feature_flags())
