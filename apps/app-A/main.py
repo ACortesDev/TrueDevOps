dfdf9a63c05f4be0ff1972e4aa086d40d68944f7
Álvaro Cortés <alvarocortesdevesa@gmail.com>
2022-02-13 21:02:33 +0100

    lets try

--- apps/app-A/main.py
+++ apps/app-A/main.py
@@ -1,26 +1,17 @@
 import os
 import time
-from typing import List, Tuple
 
 
 ANNOTATIONS_PATH = os.getenv("ANNOTATIONS_PATH")
 
 
-def parse_annotation(annotation: str) -> Tuple[str, str]:
-    k, v = annotation.split("=")
-    return k, v.strip()
-
-
-def get_feature_flags() -> List[str]:
+def get_feature_flags():
     with open(ANNOTATIONS_PATH, "r") as file:
-        annotations = [parse_annotation(line) for line in file]
-        print("annotations")
-        print(annotations)
-        feature_flags = filter(lambda x: "feature-" in x[0], annotations)
-        return list(feature_flags)
+        return [line for line in file]
 
 
 if __name__ == "__main__":
+    print("Hello World!")
     while True:
         time.sleep(5)
         print(get_feature_flags())
