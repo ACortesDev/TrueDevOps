747c39cb74db03d3c593925776ae2e1bb8e4407a
Álvaro Cortés <alvarocortesdevesa@gmail.com>
2022-02-13 20:50:04 +0100

    lets try

--- apps/app-A/main.py
+++ apps/app-A/main.py
@@ -6,13 +6,16 @@ from typing import List, Tuple
 ANNOTATIONS_PATH = os.getenv("ANNOTATIONS_PATH")
 
 
-def get_feature_flags() -> List[str]:
-    def parse_annotation(annotation: str) -> Tuple[str, str]:
-        k, v = annotation.split("=")
-        return k, v.strip()
+def parse_annotation(annotation: str) -> Tuple[str, str]:
+    k, v = annotation.split("=")
+    return k, v.strip()
 
+
+def get_feature_flags() -> List[str]:
     with open(ANNOTATIONS_PATH, "r") as file:
         annotations = [parse_annotation(line) for line in file]
+        print("annotations")
+        print(annotations)
         feature_flags = filter(lambda x: "feature-" in x[0], annotations)
         return list(feature_flags)
 
